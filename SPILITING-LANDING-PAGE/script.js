f1f5adbb9c1c23f4f151d13835b8a8114daa6aa0
Mufaddal Shakir <muffi606@gmail.com>
2021-08-14 23:50:29 +0500

    Add files via upload

---
+++ SPILITING-LANDING-PAGE/script.js
@@ -0,0 +1,7 @@
+const splitLeft = document.querySelector('.left')
+
+splitLeft.addEventListener('mouseenter', () => {
+    splitLeft.classList.add('hover-left')
+    console.log('splitLeft', splitLeft)
+ })
+

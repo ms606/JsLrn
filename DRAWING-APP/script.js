fb8992d49c154cfa87d99773e43883129ab48ff5
Mufaddal Shakir <muffi606@gmail.com>
2021-08-23 00:09:36 +0500

    Add files via upload

--- DRAWING-APP/script.js
+++ DRAWING-APP/script.js
@@ -5,40 +5,53 @@ const decrease  = document.getElementById('decrease')
 var sizeCanvas  = document.getElementById('size')
 var color       = document.getElementById('color')
 
-let size = 5 
+let size = 10 
 let colorVal = 'black'
-let pressed = false
-
+let pressed  = false
+let x 
+let y 
 
 var ctx = canvas.getContext('2d')
 
 canvas.addEventListener('mousedown', (e) => {
     pressed = true    
-})
-
-canvas.addEventListener('mousemove', (e) => {
-    if (pressed) circle(e.offsetX, e.offsetY)
+    x = e.offsetX
+    y = e.offsetY
 })
 
 canvas.addEventListener('mouseup', (e) => {
     pressed = false
+
+    x = undefined
+    y = undefined
+})
+
+canvas.addEventListener('mousemove', (e) => {
+    if (pressed) {
+        const x2 = e.offsetX;
+        const y2 = e.offsetY;
+
+        circle(e.offsetX, e.offsetY)
+        line(x, y, x2, y2)
+
+        x = x2
+        y = y2 
+    }
 })
 
 
-
 color.addEventListener('change', (e) => {
-
     colorVal = e.target.value
 })
 
 
 clear.addEventListener('click', () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height )
-        }
-    )
+    }
+)
 
 increase.addEventListener('click', () => {
-    size++
+    size += 5
     sizeCanvas.innerHTML = size
 })    
 
@@ -52,6 +65,14 @@ decrease.addEventListener('click', () => {
 })
 
 
+function line(x, y, x2, y2){
+    ctx.beginPath()
+    ctx.moveTo(x,y);
+    ctx.lineTo(x2,y2);
+    ctx.strokeStyle = colorVal;
+    ctx.lineWidth = size * 2;    
+    ctx.stroke();
+}
 
 function circle(x, y){
     console.log(colorVal)
